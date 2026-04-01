21bbf2c4539cbc466c31000429dfb48020e325e1
aishwaryakulkarni-de <aishwarya.kulkarni@magnadata.in>
2026-04-01 10:49:19 +0530

    Added feature: login functionality

--- emp_app/views.py
+++ emp_app/views.py
@@ -1,23 +1,55 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import Employee, Role, Department
 from datetime import datetime
 from django.db.models import Q
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserLoginForm
 
 
-# Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 
+def register_view(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserLoginForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required
 def all_emp(request):
     emps = Employee.objects.all()
     context = {
         'emps': emps
     }
-    print(context)
     return render(request, 'view_all_emp.html', context)
 
 
+@login_required
 def add_emp(request):
     if request.method == 'POST':
         first_name = request.POST['first_name']
@@ -27,30 +59,32 @@ def add_emp(request):
         phone = int(request.POST['phone'])
         dept = int(request.POST['dept'])
         role = int(request.POST['role'])
-        new_emp = Employee(first_name= first_name, last_name=last_name, salary=salary, bonus=bonus, phone=phone, dept_id = dept, role_id = role, hire_date = datetime.now())
+        new_emp = Employee(first_name=first_name, last_name=last_name, salary=salary, bonus=bonus, phone=phone, dept_id=dept, role_id=role, hire_date=datetime.now())
         new_emp.save()
         return HttpResponse('Employee added Successfully')
-    elif request.method=='GET':
+    elif request.method == 'GET':
         return render(request, 'add_emp.html')
     else:
         return HttpResponse("An Exception Occured! Employee Has Not Been Added")
 
 
-def remove_emp(request, emp_id = 0):
+@login_required
+def remove_emp(request, emp_id=0):
     if emp_id:
         try:
             emp_to_be_removed = Employee.objects.get(id=emp_id)
             emp_to_be_removed.delete()
             return HttpResponse("Employee Removed Successfully")
-        except:
+        except Exception:
             return HttpResponse("Please Enter A Valid EMP ID")
     emps = Employee.objects.all()
     context = {
         'emps': emps
     }
-    return render(request, 'remove_emp.html',context)
+    return render(request, 'remove_emp.html', context)
 
 
+@login_required
 def filter_emp(request):
     if request.method == 'POST':
         name = request.POST['name']
@@ -58,11 +92,11 @@ def filter_emp(request):
         role = request.POST['role']
         emps = Employee.objects.all()
         if name:
-            emps = emps.filter(Q(first_name__icontains = name) | Q(last_name__icontains = name))
+            emps = emps.filter(Q(first_name__icontains=name) | Q(last_name__icontains=name))
         if dept:
-            emps = emps.filter(dept__name__icontains = dept)
+            emps = emps.filter(dept__name__icontains=dept)
         if role:
-            emps = emps.filter(role__name__icontains = role)
+            emps = emps.filter(role__name__icontains=role)
 
         context = {
             'emps': emps
